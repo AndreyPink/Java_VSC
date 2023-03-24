350160ffb899e51d5de0d4ba7e567a0518d3de65
ANDREY PINK <andreypink@bk.ru>
2023-03-24 13:01:04 +0300

    update

--- HW/hw_1/ex_2.java
+++ HW/hw_1/ex_2.java
@@ -3,5 +3,16 @@
 package HW.hw_1;
 
 public class ex_2 {
-    
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 1; i < 1000; i++) {
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0)
+                    count++;
+            }
+            if (count == 2)
+                System.out.printf("%d ", i);
+            count = 0;
+        }
+    }
 }
