1f7cdf91b6e738f9155b8df0b120e89c9dba4526
ANDREY PINK <andreypink@bk.ru>
2023-03-24 13:27:33 +0300

    update

--- HW/hw_1/ex_3.java
+++ HW/hw_1/ex_3.java
@@ -10,13 +10,26 @@ import java.util.Scanner;
 public class ex_3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Введите число: ");
-        int num = sc.nextInt();
-        System.out.println("Введите число: ");
-        int num = sc.nextInt();
-        System.out.println("Введите число: ");
-        int num = sc.nextInt();
+        System.out.println("Введите число 1: ");
+        int num1 = sc.nextInt();
+        System.out.println("Введите знак: ");
+        String sym = sc.next();
+        System.out.println("Введите число 2: ");
+        int num2 = sc.nextInt();
 
+        System.out.println("/" + sym + "/");
 
+        if (sym == "+") {
+            System.out.printf("%d + %d = %d", num1, num2, num1 + num2);
+        } else if (sym == "-") {
+            System.out.printf("%d - %d = %d", num1, num2, num1 - num2);
+        } else if (sym == "*") {
+            System.out.printf("%d * %d = %d", num1, num2, num1 * num2);
+        } else if (sym == "/") {
+            System.out.printf("%d / %d = %d", num1, num2, num1 / num2);
+        } else {
+            System.out.println("Ошибка ввода");
+        }
+        sc.close();
     }
 }
