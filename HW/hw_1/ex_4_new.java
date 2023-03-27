5496b07eb1b45c722d04ab891fdd94c3ad0787ae
ANDREY PINK <andreypink@bk.ru>
2023-03-27 04:05:32 +0300

    update

--- HW/hw_1/ex_4_new.java
+++ HW/hw_1/ex_4_new.java
@@ -3,6 +3,23 @@ package HW.hw_1;
 import java.util.Scanner;
 
 public class ex_4_new {
+
+    public static int convert(String num, int j) {
+        int[] Mas = new int[num.length()];
+        for (int i = 0; i < num.length(); i++) {
+
+            if (Character.isDigit(num.toCharArray()[i]))
+                Mas[i] = Character.digit(num.toCharArray()[i], 10);
+            else
+                Mas[i] = j;
+        }
+        StringBuilder string = new StringBuilder();
+        for (int i = 0; i < Mas.length; i++)
+            string.append(Mas[i]);
+        int newNum = Integer.parseInt(string.toString());
+        return newNum;
+    }
+
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
@@ -14,50 +31,11 @@ public class ex_4_new {
         System.out.println("Введите двузначную сумму слагаемых: ");
         String c = sc.nextLine();
 
-        int[] MasA = new int[a.length()];
-        int[] MasB = new int[b.length()];
-        int[] MasC = new int[c.length()];
-
         for (int j = 0; j < 10; j++) {
 
-            for (int i = 0; i < a.length(); i++) {
-
-                if (Character.isDigit(a.toCharArray()[i]))
-                    MasA[i] = Character.digit(a.toCharArray()[i], 10);
-                else
-                    MasA[i] = j;
-            }
-            StringBuilder stringA = new StringBuilder();
-            for(int i=0; i < MasA.length; i++)
-            stringA.append(MasA[i]);
-            int newA = Integer.parseInt(stringA.toString());
-
-            for (int i = 0; i < b.length(); i++) {
-
-                if (Character.isDigit(b.toCharArray()[i]))
-                    MasB[i] = Character.digit(b.toCharArray()[i], 10);
-                else
-                    MasB[i] = j;
-            }
-            StringBuilder stringB = new StringBuilder();
-            for(int i=0; i < MasB.length; i++)
-            stringB.append(MasB[i]);
-            int newB = Integer.parseInt(stringB.toString());
-
-            for (int i = 0; i < c.length(); i++) {
-
-                if (Character.isDigit(c.toCharArray()[i]))
-                    MasC[i] = Character.digit(c.toCharArray()[i], 10);
-                else
-                    MasC[i] = j;
-            }
-            StringBuilder stringC = new StringBuilder();
-            for(int i=0; i < MasC.length; i++)
-            stringC.append(MasC[i]);
-            int newC = Integer.parseInt(stringC.toString());
-
-            if (newA + newB == newC) {
-                System.out.printf("Решение данного уравнения: %d + %d = %d", newA, newB, newC);
+            if (convert(a, j) + convert(b, j) == convert(c, j)) {
+                System.out.printf("Решение данного уравнения: %d + %d = %d", convert(a, j), convert(b, j),
+                        convert(c, j));
                 check = true;
             }
         }
