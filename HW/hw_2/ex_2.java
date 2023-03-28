20e59e3f8597e03729f76ee12b414dd608005c3d
ANDREY PINK <andreypink@bk.ru>
2023-03-28 18:34:04 +0300

    update

--- HW/hw_2/ex_2.java
+++ HW/hw_2/ex_2.java
@@ -13,6 +13,35 @@
 
 package HW.hw_2;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 public class ex_2 {
-    
+
+    public static String readFile(String path) {
+        String line = "";
+        try {
+            String str;
+            File file = new File(path);
+            BufferedReader bf = new BufferedReader(new FileReader(file));
+            while ((str=bf.readLine())!= null) {
+                line += str + "\n";
+            }
+            bf.close();
+            return line;
+        } catch (Exception e) {
+            return line;
+        }
+    }
+
+    public static void parser(String line) {
+        JSONParser parser = new JSONParser();
+
+    }
+
+    public static void main(String[] args) {
+        String path = "HW/hw_2/school.txt";
+        System.out.println(readFile(path));
+    }
 }
---
+++ Lectures/temp.java
@@ -0,0 +1,11 @@
+package Lectures;
+
+/**
+ * temp
+ */
+public class temp {
+
+    public static void main(String[] args) {
+        System.out.print("\033[H\033[J");
+    }
+}
