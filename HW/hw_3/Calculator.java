ab5be7be1192e35e731870f11a82643e7f933ea4
ANDREY PINK <andreypink@bk.ru>
2023-03-29 13:35:35 +0300

    update

---
+++ HW/hw_3/Calculator.java
@@ -0,0 +1,41 @@
+package HW.hw_3;
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Выберите операцию: ");
+        System.out.println("1. Сложение");
+        System.out.println("2. Вычитание");
+        System.out.println("3. Умножение");
+        System.out.println("4. Деление");
+        int operation = scanner.nextInt();
+
+        double result = 0;
+        switch (operation) {
+            case 1:
+                result = num1 + num2;
+                break;
+            case 2:
+                result = num1 - num2;
+                break;
+            case 3:
+                result = num1 * num2;
+                break;
+            case 4:
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Вы выбрали неверную операцию.");
+                break;
+        }
+        scanner.close();
+        System.out.println("Результат: " + result);
+    }
+}
