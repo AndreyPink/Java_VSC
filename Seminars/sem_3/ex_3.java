8c140d68d7ae6d553671a6b3c3278a16623b80cb
ANDREY PINK <andreypink@bk.ru>
2023-03-31 11:44:42 +0300

    update

---
+++ Seminars/sem_3/ex_3.java
@@ -0,0 +1,22 @@
+// Создать список типа ArrayList
+// Поместить в него как строки, так и целые числа.
+// Пройти по списку, найти и удалить целые числа.
+
+package Seminars.sem_3;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+
+public class ex_3 {
+    public static void main(String[] args) {
+        ArrayList<Object> list = new ArrayList<>(Arrays.asList(1, "y", 2, "t", 3, "u", 4, 5));
+        Iterator<Object> iter = list.iterator();
+        while (iter.hasNext()) {
+            if (iter.next() instanceof Integer) {
+                iter.remove();
+            }
+        }
+        System.out.println(list);
+    }
+}
