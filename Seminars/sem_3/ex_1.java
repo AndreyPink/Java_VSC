30420c9ea9b35f87314b2fe8c16ef73103d4f193
ANDREY PINK <andreypink@bk.ru>
2023-03-31 11:27:53 +0300

    update

--- Seminars/sem_3/ex_1.java
+++ Seminars/sem_3/ex_1.java
@@ -1,11 +1,23 @@
-
-
-
+// Заполнить список десятью случайными числами.
+// Отсортировать список методом sort() и вывести его на экран.
 
 package Seminars.sem_3;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Random;
+
 public class ex_1 {
     public static void main(String[] args) {
-        
+        ArrayList<Integer> list = new ArrayList<>();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(0,100));
+        }
+        System.out.println(list);
+        list.sort(null);
+        System.out.println(list);
+        list.sort(Comparator.reverseOrder());
+        System.out.println(list);
     }
 }
--- Seminars/sem_3/ex_2.java
+++ Seminars/sem_3/ex_2.java
@@ -1,11 +1,32 @@
-
-
-
+// Заполнить список названиями планет Солнечной системы в произвольном порядке с повторениями.
+// Вывести название каждой планеты и количество его повторений в списке.
 
 package Seminars.sem_3;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ex_2 {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        ArrayList<String> planets = new ArrayList<>();
+        String[] currentPlanets = { "Mercury", "Venera", "Saturn", "Uran",
+                "Neptun", "Earth", "Jupiter" };
+
+        for (int i = 0; i < 10; i++) {
+            planets.add(currentPlanets[random.nextInt(currentPlanets.length - 1)]);
+        }
+        System.out.println(planets);
+        int count = 1;
+        planets.sort(null);
+        for (int i = 1; i < planets.size(); i++) {
+            if (planets.get(i) == planets.get(i - 1)) {
+                count += 1;
+            } else {
+                System.out.println(planets.get(i - 1) + " " + count);
+                count = 1;
+            }
+        }
+        System.out.println(planets.get(planets.size() - 1) + " " + count);
     }
 }
