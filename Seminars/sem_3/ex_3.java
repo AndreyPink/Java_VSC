24eba708120f455c6f1d57a3d1f8f135cb183a31
ANDREY PINK <andreypink@bk.ru>
2023-03-31 12:05:36 +0300

    update

--- Seminars/sem_3/ex_3.java
+++ Seminars/sem_3/ex_3.java
@@ -11,10 +11,23 @@ import java.util.Iterator;
 public class ex_3 {
     public static void main(String[] args) {
         ArrayList<Object> list = new ArrayList<>(Arrays.asList(1, "y", 2, "t", 3, "u", 4, 5));
-        Iterator<Object> iter = list.iterator();
-        while (iter.hasNext()) {
-            if (iter.next() instanceof Integer) {
-                iter.remove();
+        // Iterator<Object> iter = list.iterator();
+        // while (iter.hasNext()) {
+        // if (iter.next() instanceof Integer) {
+        // iter.remove();
+        // }
+        // }
+
+        // for (int i = list.size()-1; i >= 0; i--) {
+        // if (list.get(i) instanceof Integer) {
+        // list.remove(i);
+        // }
+        // }
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) instanceof Integer) {
+                list.remove(i);
+                i--;
             }
         }
         System.out.println(list);
