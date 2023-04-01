0b2eb601b587b5772bbfd7fa5ade91ec325a44bf
ANDREY PINK <andreypink@bk.ru>
2023-04-01 14:49:10 +0300

    update

--- HW/hw_3/ex_2.java
+++ HW/hw_3/ex_2.java
@@ -2,8 +2,22 @@
 
 package HW.hw_3;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ex_2 {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(random.nextInt(10));
+        }
+        System.out.println("Произвольный список чисел: " + list);
+        for (int i = list.size()-1; i >= 0; i--) {
+                if (list.get(i)%2 == 0) {
+                list.remove(i);
+            }
+        }
+        System.out.println("Cписок c удаленными четными числами: " + list);
     }
 }
