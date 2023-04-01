a88b62450a13ab7f04f03552667e6303de5328e9
ANDREY PINK <andreypink@bk.ru>
2023-04-01 17:37:27 +0300

    update

--- HW/hw_3/ex_1.java
+++ HW/hw_3/ex_1.java
@@ -1,5 +1,3 @@
-// (Дополнительное) Реализовать алгоритм сортировки слиянием
-
 package HW.hw_3;
 
 import java.util.Arrays;
@@ -7,50 +5,29 @@ import java.util.Random;
 
 public class ex_1 {
 
-    public void mergeSort(int[] arr) {
-        if (arr.length < 2) {
-            return;
-        }
-        int mid = arr.length / 2;
-        int[] leftArr = new int[mid];
-        int[] rightArr = new int[arr.length - mid];
-
-        for (int i = 0; i < mid; i++) {
-            leftArr[i] = arr[i];
-        }
-        for (int i = mid; i < arr.length; i++) {
-            rightArr[i - mid] = arr[i];
-        }
-        mergeSort(leftArr);
-        mergeSort(rightArr);
-        merge(leftArr, rightArr, arr);
+    public static int[] mergeSort(int[] array) {
+        if (array.length <= 1) return array;
+        int[] left = Arrays.copyOfRange(array, 0, array.length / 2);
+        int[] right = Arrays.copyOfRange(array, left.length, array.length);
+        return merge(mergeSort(left), mergeSort(right));
     }
 
-    public void merge(int[] leftArr, int[] rightArr, int[] resultArray) {
-        int leftI = 0, rightI = 0, resI = 0;
-        while (leftI < leftArr.length && rightI < rightArr.length) {
-            if (leftArr[leftI] <= rightArr[rightI]) {
-                resultArray[resI] = leftArr[leftI];
-                leftI++;
-            } else {
-                resultArray[resI] = rightArr[rightI];
-                rightI++;
-            }
-            resI++;
-        }
-        while (leftI < leftArr.length) {
-            resultArray[resI] = leftArr[leftI];
-            leftI++;
-            resI++;
-        }
+private static int[] merge(int[] left, int[] right) {
+        int resIn = 0, leftIn = 0, rightIn = 0;
+        int[] result = new int[left.length + right.length];
 
-        while (rightI < rightArr.length) {
-            resultArray[resI] = rightArr[rightI];
-            rightI++;
-            resI++;
-        }
+        while (leftIn < left.length && rightIn < right.length)
+            if (left[leftIn] < right[rightIn])
+                result[resIn++] = left[leftIn++];
+            else result[resIn++] = right[rightIn++];
+
+        while (resIn < result.length)
+            if (leftIn != left.length)
+                result[resIn++] = left[leftIn++];
+            else result[resIn++] = right[rightIn++];
+
+        return result;
     }
-
     public static void main(String[] args) {
         Random random = new Random();
         int[] array = new int[10];
@@ -58,8 +35,6 @@ public class ex_1 {
             array[i] = random.nextInt(100);
         }
         System.out.println("Первоначальный массив: " + Arrays.toString(array));
-        ex_1 m = new ex_1();
-        m.mergeSort(array);
-        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+        System.out.println("Отсортированный массив: " + Arrays.toString(mergeSort(array)));
     }
 }
