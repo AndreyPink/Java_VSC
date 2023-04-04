bb362d9bab6ffa3fd57ce235c0647a9928db5548
ANDREY PINK <andreypink@bk.ru>
2023-04-04 12:15:53 +0300

    update

--- HW/hw_4/ex_1.java
+++ HW/hw_4/ex_1.java
@@ -1,9 +1,23 @@
 // Пусть дан LinkedList с несколькими элементами. 
 // Реализуйте метод(не void), который вернет “перевернутый” список.
 
-
 package HW.hw_4;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 public class ex_1 {
-    
+    public static void main(String[] args) {
+        LinkedList<Integer> ll = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+        System.out.println(reversArray(ll));
+    }
+
+    public static ArrayList reversArray(LinkedList ll) {
+        ArrayList<Integer> array = new ArrayList<>();
+        for (int i = ll.size() - 1; i >= 0; i--) {
+            array.add((Integer) ll.get(i));
+        }
+        return array;
+    }
 }
