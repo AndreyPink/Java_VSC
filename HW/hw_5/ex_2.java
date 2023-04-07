3934ad39a9e53cbe83f63a4250fdc4847334f9c8
ANDREY PINK <andreypink@bk.ru>
2023-04-07 14:49:03 +0300

    update

--- HW/hw_5/ex_2.java
+++ HW/hw_5/ex_2.java
@@ -28,10 +28,8 @@ import static java.util.Collections.reverseOrder;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
