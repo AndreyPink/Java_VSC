7d6a0f7494b9f1f9cca797ec813656d1e298678d
ANDREY PINK <andreypink@bk.ru>
2023-04-07 11:50:50 +0300

    update

--- Seminars/sem_5/ex_1.java
+++ Seminars/sem_5/ex_1.java
@@ -1,10 +1,38 @@
+// Создать структуру для хранения Номеров паспортов и Фамилий сотрудников организации.
 
+// 123456 Иванов
+
+// 321456 Васильев
+
+// 234561 Петрова
+
+// 234432 Иванов
+
+// 654321 Петрова
+
+// 345678 Иванов
+
+// Вывести данные по сотрудникам с фамилией Иванов.
 
 
 package Seminars.sem_5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ex_1 {
     public static void main(String[] args) {
-        
+        Map<Integer, String> hm = new HashMap<>();
+        hm.put(123456, "Иванов");
+        hm.put(321456, "Васильев");
+        hm.put(234561, "Петрова");
+        hm.put(234432, "Иванов");
+        hm.put(654321, "Петрова");
+        hm.put(345678, "Иванов");
+        for (var item: hm.entrySet()) {
+            if (item.getValue().equals("Иванов")){
+                System.out.println(item.getKey() + " " + item.getValue());
+            }
+        }
     }
 }
--- Seminars/sem_5/ex_2.java
+++ Seminars/sem_5/ex_2.java
@@ -1,10 +1,54 @@
+// Даны 2 строки, написать метод, который вернет true, если эти строки являются изоморфными и false, если нет.
+//  Строки изоморфны, если одну букву в первом слове можно заменить на некоторую букву во втором слове, при этом
 
+// 1. повторяющиеся буквы одного слова меняются на одну и ту же букву с сохранением порядка следования. 
+// (Например, add - egg изоморфны)
+// 2. буква может не меняться, а остаться такой же. (Например, note - code)
+
+// Пример 1:
+
+// Input: s = "foo", t = "baar"
+// f: b
+// o: a
+
+
+// Output: false
+
+// Пример 2:
+
+// Input: s = "paper", t = "title"
+// p: t
+// a: i
+// e: l
+// r: e
+
+// Output: true
 
 
 package Seminars.sem_5;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ex_2 {
     public static void main(String[] args) {
+        String st1 = "paper";
+        String st2 = "title";
+        if (st1.length() != st2.length())
+            System.out.println(false);
+        else 
+            System.out.println(isIsomorph(st1, st2));
+    }
+
         
+    public static boolean isIsomorph(String str1, String str2 ){
+        Map<Character, Character> hm = new HashMap<>();
+        for (var i = 0; i < str1.length(); i++) {
+            if (hm.containsKey(str1.charAt(i)) && hm.get(str1.charAt(i)) != str2.charAt(i))
+                    return false;
+            else
+                hm.put(str1.charAt(i), str2.charAt(i));
+        }
+        return true;
     }
 }
