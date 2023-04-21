23fa3d846e8d0bda417c059faff9ea93a70b8c01
ANDREY PINK <andreypink@bk.ru>
2023-04-21 12:50:22 +0300

    update

--- OOP/Seminars/sem_3/ex_2.java
+++ OOP/Seminars/sem_3/ex_2.java
@@ -8,40 +8,52 @@ public class ex_2 {
 
     private static Random random = new Random();
 
-    static String[] names = new String[] { "Ivan", "Petr", "Vasya", "Oleg", "Mikhail" };
-    static String[] surNames = new String[] { "Ivanov", "Petrov", "Vasilyev", "Sokolov", "Vorobyev" };
-
-    static Worker generateWorker() {
+    static Employee generateEmployee() {
+        String[] names = new String[] { "Иван", "Петр", "Василий", "Олег", "Михаил" };
+        String[] surNames = new String[] { "Иванов", "Петров", "Васильев", "Соколов", "Воробьев" };
         int salary = random.nextInt(200, 300);
         int index = random.nextInt(30, 50);
-        return new Worker(names[random.nextInt(5)], surNames[random.nextInt(5)], salary * index);
-    }
+        int age = random.nextInt(25, 50);
+        if (random.nextBoolean())
+            return new Worker(names[random.nextInt(5)], surNames[random.nextInt(5)], age, salary * index);
+        else
+            return new Freelancer(names[random.nextInt(5)], surNames[random.nextInt(5)], age, salary * index / 100);
 
-    static Freelancer generateFreelancer() {
-        int salary = random.nextInt(200, 300);
-        int index = random.nextInt(30, 50);
-        return new Freelancer(names[random.nextInt(5)], surNames[random.nextInt(5)], salary * index / 100);
     }
 
     public static void main(String[] args) {
 
         Employee[] employees = new Employee[5];
         for (int i = 0; i < employees.length; i++) {
-            if (random.nextBoolean())
-                employees[i] = generateWorker();
-            else
-                employees[i] = generateFreelancer();
+            employees[i] = generateEmployee();
+
         }
         for (Employee employee : employees) {
             System.out.println(employee);
         }
 
-        // Arrays.sort(employees, new SalaryComparator());
-        // Arrays.sort(employees, new NameComparator());
-        System.out.println("------------------------------------");
-        System.out.println("Отсортированный список:\n");
+        System.out.println("\n------------------------------------");
+        System.out.println("Сортировка по ежемесячному доходу:\n");
 
-        Arrays.sort(employees);
+        Arrays.sort(employees, new SalaryComparator());
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+        System.out.println("\n------------------------------------");
+        System.out.println("Сортировка по имени:\n");
+
+        Arrays.sort(employees, new NameComparator());
+
+        for (Employee employee : employees) {
+            System.out.println(employee);
+        }
+
+        System.out.println("\n------------------------------------");
+        System.out.println("Сортировка по возрасту:\n");
+
+        Arrays.sort(employees, new AgeComparator());
 
         for (Employee employee : employees) {
             System.out.println(employee);
@@ -51,35 +63,45 @@ public class ex_2 {
 
 }
 
-// class SalaryComparator implements Comparator<Employee> {
+class SalaryComparator implements Comparator<Employee> {
 
-// @Override
-// public int compare(Employee o1, Employee o2) {
+    @Override
+    public int compare(Employee o1, Employee o2) {
 
-// // return o1.calculateSalary() == o2.calculateSalary() ? 0 :
-// // (o1.calculateSalary() > o2.calculateSalary() ? 1 : -1);
-// return Double.compare(o1.calculateSalary(), o2.calculateSalary());
-// }
-// }
+        // return o1.calculateSalary() == o2.calculateSalary() ? 0 :
+        // (o1.calculateSalary() > o2.calculateSalary() ? 1 : -1);
+        return Double.compare(o1.calculateSalary(), o2.calculateSalary());
+    }
+}
 
-// class NameComparator implements Comparator<Employee> {
+class NameComparator implements Comparator<Employee> {
 
-// @Override
-// public int compare(Employee o1, Employee o2) {
-// return o1.name.compareTo(o2.name);
-// }
-// }
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return o1.name.compareTo(o2.name);
+    }
+}
 
-abstract class Employee implements Comparable<Employee> {
+class AgeComparator implements Comparator<Employee> {
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        return Double.compare(o1.age, o2.age);
+    }
+}
+
+abstract class Employee {
 
     protected String name;
     protected String surName;
     protected double salary;
+    protected int age;
 
-    public Employee(String name, String surName, double salary) {
+    public Employee(String name, String surName, int age, double salary) {
         this.name = name;
         this.surName = surName;
         this.salary = salary;
+        this.age = age;
     }
 
     public abstract double calculateSalary();
@@ -89,23 +111,24 @@ abstract class Employee implements Comparable<Employee> {
         return String.format("Сотрудник: %s %s; среднемесячная з/п: %.2f", name, surName, salary);
     }
 
-    @Override
-    public int compareTo(Employee o) {
-        return Double.compare(calculateSalary(), o.calculateSalary());
-    }
+    // @Override
+    // public int compareTo(Employee o) {
+    // return Double.compare(calculateSalary(), o.calculateSalary());
+    // }
 
 }
 
 class Worker extends Employee {
 
-    public Worker(String name, String surName, double salary) {
-        super(name, surName, salary);
+    public Worker(String name, String surName, int age, double salary) {
+        super(name, surName, age, salary);
 
     }
 
     @Override
     public String toString() {
-        return String.format("Сотрудник: %s %s; Рабочий; фиксированная з/п за месяц: %.2f", name, surName, salary);
+        return String.format("Сотрудник: %s %s; %d лет; Рабочий; фиксированная з/п за месяц: %.2f", name, surName, age,
+                salary);
     }
 
     @Override
@@ -118,8 +141,8 @@ class Worker extends Employee {
 
 class Freelancer extends Employee {
 
-    public Freelancer(String name, String surName, double salary) {
-        super(name, surName, salary);
+    public Freelancer(String name, String surName, int age, double salary) {
+        super(name, surName, age, salary);
 
     }
 
@@ -130,7 +153,9 @@ class Freelancer extends Employee {
 
     @Override
     public String toString() {
-        return String.format("Сотрудник: %s %s; Фрилансер; почасовая з/п: %.2f; средняя з/п за месяц (100 часов): %.2f", name, surName, salary, calculateSalary());
+        return String.format(
+                "Сотрудник: %s %s; %d лет; Фрилансер; почасовая з/п: %.2f; средняя з/п за месяц (100 часов): %.2f",
+                name, surName, age, salary, calculateSalary());
     }
 
 }
